a12070dd51a293fc1e4405fa9f04ddb7f87eaa23
koray-eren <koray.eren@outlook.com>
2022-01-31 11:08:46 +1100

    deleted temp file, fixed formatting

--- main.rb
+++ main.rb
@@ -1,22 +1,5 @@
 require_relative "robot.rb"
 require_relative "grid.rb"
-# 5 x 5 grid
-# [04, 14, 24, 34, 44]
-# [03, 13, 23, 33, 43]
-# [02, 12, 22, 32, 42]
-# [01, 11, 21, 31, 41]
-# [00, 10, 20, 30, 40]
-
-# calculate next square (place or move)
-# verify next square (is it on the grid)
-# apply next square (if valid, move the robot)
-
-# VARS
-# facing = [NORTH, EAST, SOUTH, WEST]
-# position = []
-
-# puts("enter a command")
-# input = gets.chomp.split(' ')
 
 grid = Grid.new
 grid.start_program
--- tempCodeRunnerFile.rb
+++
@@ -1,3 +0,0 @@
-        input[1] = input[1].split(',')
-        command = input[0].downcase
-        parameters = input[1]
